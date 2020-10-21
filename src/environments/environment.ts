d2ed9c2a97b8773cfb72b81d36ec86d3b9e18a7c
mashudu <mselepe747@gmail.com>
2020-10-21 08:33:05 +0200

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,17 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase :{
-    apiKey: "AIzaSyCoHpy5sAxn0fgCGfzGqDs91Uo5JbAQO3w",
-  authDomain: "database-b7514.firebaseapp.com",
-  databaseURL: "https://database-b7514.firebaseio.com",
-  projectId: "database-b7514",
-  storageBucket: "database-b7514.appspot.com",
-  messagingSenderId: "976931604537",
-  appId: "1:976931604537:web:92de1d3dd9f5867a9725e8",
-  measurementId: "G-DFN3PH975H"
 
-  }
 };
 
 /*
